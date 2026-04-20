d21176f93bb06b6cf76c8736f4a5d46bd18435d3
Aman Prajapat <ap871103@gmail.com>
2026-04-20 13:33:34 +0530

    fix duplicate imports and App.jsx conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,9 @@ import Navbar from "./Components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Cart from "./pages/Cart/Cart";
-<<<<<<< HEAD
+
 import PlaceOrder from "./pages/placeorder/PlaceOrder";
-=======
-import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
->>>>>>> b7e9f35 (update frontend fixes)
+
 import Footer from "./Components/Footer/Footer";
 import ExploreMenu from "./Components/ExploreMenu/ExploreMenu";
 import AppDownload from "./Components/AppDownload/AppDownload";
